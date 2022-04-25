1f3c115186814ee72c9a96438171c12a01c6905a
HuyTV <tranvanhuy0202@gmail.com>
2022-04-25 16:17:27 +0700

    json editor

---
+++ resources/js/src/components/input/JsonEditor.vue
@@ -0,0 +1,50 @@
+<template lang="pug">
+#jsoneditor
+</template>
+
+<script setup>
+import {onMounted} from 'vue';
+import JSONEditor from '../../assets/js/jsoneditor.min';
+
+let editor = null;
+
+const exportJson = () => {
+  if (editor) {
+    try {
+      return JSON.stringify(editor.get());
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  }
+}
+
+defineExpose({
+  exportJson,
+})
+
+onMounted(() => {
+  // create the editor
+  const container = document.getElementById("jsoneditor");
+  const options = {
+    mode: "code",
+  };
+  editor = new JSONEditor(container, options);
+})
+
+</script>
+
+<style>
+@import '../../assets/css/jsoneditor.min.css';
+#jsoneditor {
+  width: 100%;
+  height: 600px;
+}
+button.jsoneditor-sort,
+button.jsoneditor-transform,
+button.jsoneditor-repair,
+button.jsoneditor-undo,
+button.jsoneditor-redo,
+a.jsoneditor-poweredBy {
+  display: none;
+}
+</style>
